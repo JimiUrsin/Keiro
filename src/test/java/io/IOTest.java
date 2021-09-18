e7a5cb4bcd55c56d5e94a1ed9305fa834a039ba6
JimiUrsin <jimi.ursin@gmail.com>
2021-09-18 15:53:57 +0300

    Added tests

---
+++ src/test/java/io/IOTest.java
@@ -0,0 +1,67 @@
+package io;
+
+import com.jhursin.keiro.io.FileToImage;
+import com.jhursin.keiro.io.ImageToFile;
+import java.awt.image.BufferedImage;
+
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class IOTest {
+    /**
+     * Makes a 100x100 dummy BufferedImage with an alternating black-and-white pattern
+     * @return Dummy BufferedImage with aforementioned pattern
+     */
+    private static BufferedImage makeDummyImage() {
+        BufferedImage dummy = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
+        boolean white = true;
+        for(int y = 0; y < 100; y++) {
+            white = !white;
+            for(int x = 0; x < 100; x++) {
+                if (white) {
+                    dummy.setRGB(x, y, 0xFFFFFFFF);
+                } else {
+                    dummy.setRGB(x, y, 0xFF000000);
+                }
+                white = !white;
+            }
+        }
+        return dummy;
+    }
+    
+    BufferedImage bimg;
+    
+    @Before
+    public void setUp() {
+        bimg = makeDummyImage();
+    }
+    
+    @Test
+    public void testReadingFromFile() {
+        BufferedImage checkers = FileToImage.readFile("resources\\test\\checkers.png");
+        assertFalse("File reading was not successful", checkers.getWidth() == 1 && checkers.getHeight() == 1);
+    }
+    
+    @Test
+    public void testWritingAndReadingMatchesOriginal() {
+        String filename = "resources\\test\\temp_checkers.png";
+        ImageToFile.drawToFile(bimg, filename);
+        BufferedImage bimg2 = FileToImage.readFile(filename);
+        
+        assertEquals("Image differs in width from original", bimg.getWidth(), bimg2.getWidth());
+        assertEquals("Image differs in height from original", bimg.getHeight(), bimg2.getHeight());
+        
+        for(int y = 0; y < bimg.getHeight(); y++) {
+            for(int x = 0; x < bimg.getWidth(); x++) {
+                if (bimg.getRGB(x, y) != bimg2.getRGB(x, y)) {
+                    fail(String.format("Pixels at (%d, %d) were different, expected %08X, was %08X", x, y, bimg.getRGB(x, y), bimg2.getRGB(x, y)));
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/path/PathTest.java
@@ -0,0 +1,67 @@
+package path;
+
+import com.jhursin.keiro.logic.Grid;
+import com.jhursin.keiro.logic.Node;
+import com.jhursin.keiro.logic.Path;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PathTest {
+    
+    /**
+     * Make and grid full of empty nodes with a start node in the upper left corner
+     * and an end node in the bottom right
+     * @param width
+     * @param height
+     * @return 
+     */
+    private Grid makeEmptyGrid(int width, int height) {
+        Grid g = new Grid(width, height);
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                g.nodes[y][x] = Node.EMPTY;
+            }
+        }
+        g.setStart(0, 0);
+        g.setEnd(width - 1, height - 1);
+        
+        return g;
+    }
+    
+    Grid g;
+    
+    @Before
+    public void setUp() {
+        g = makeEmptyGrid(10, 10);
+    }
+    
+    @Test
+    public void testAStar() {
+        Path.setDiagonal(false);
+        int length = Path.solveAStar(g);
+        assertEquals("Path was not shortest possible", length, 19);
+    }
+    
+    @Test
+    public void testAStarDiagonal() {
+        Path.setDiagonal(true);
+        int length = Path.solveAStar(g);
+        assertEquals("Path was not shortest possible", length, 10);
+    }
+    
+    @Test
+    public void testAStarWithObstacles() {
+        Path.setDiagonal(true);
+        for(int y = 1; y < 9; y++) {
+            g.nodes[y][1] = Node.BLOCKED;
+        }
+        for(int x = 1; x < 9; x++) {
+            g.nodes[1][x] = Node.BLOCKED;
+        }
+        int length = Path.solveAStar(g);
+        
+        assertEquals("Path was not shortest possible", length, 18);        
+    }
+}
